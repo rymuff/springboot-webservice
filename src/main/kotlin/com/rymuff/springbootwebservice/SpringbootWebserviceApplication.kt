0499068a7fcef4137142b5efed4c712b520e70cb
rymuff <rymuff@gmail.com>
2021-01-29 00:26:39 +0900

    Initial commit

---
+++ src/main/kotlin/com/rymuff/springbootwebservice/SpringbootWebserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.rymuff.springbootwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootWebserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootWebserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/rymuff/springbootwebservice/SpringbootWebserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rymuff.springbootwebservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
